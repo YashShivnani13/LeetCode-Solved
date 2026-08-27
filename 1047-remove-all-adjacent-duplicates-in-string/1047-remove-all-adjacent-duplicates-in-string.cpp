cffbc185aa1932488fca1d78e50ade0befdbef72
YashShivnani13 <shivnaniyash2@gmail.com>
2026-08-27 16:01:04 +0530

    Time: 6 ms (56.01%), Space: 13.7 MB (74.6%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string temp = "";
+        for(int i=0; i<s.size(); i++){
+            
+            if(!temp.empty() && temp.back() == s[i]){
+                temp.pop_back();
+            }
+            else{
+                temp.push_back(s[i]);
+            }
+            
+        }
+        return temp;
+    }
+};
