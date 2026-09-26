9e5bcde73912c1d55207cd7cc457dbd10e67361c
YashShivnani13 <shivnaniyash2@gmail.com>
2026-09-26 15:52:34 +0530

    Time: 0 ms (100%), Space: 8.9 MB (80.86%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string ans = "";
+        int count = 0;
+
+        for(int i=0; i<s.size(); i++){
+
+            if(s[i]=='('){
+
+                if(count>0){
+                    ans.push_back(s[i]);
+                }
+                count++;
+            }
+            else{
+                count--;
+                if(count>0){
+                    ans.push_back(s[i]);
+                }
+            }
+        }
+        return ans;
+    }
+};
